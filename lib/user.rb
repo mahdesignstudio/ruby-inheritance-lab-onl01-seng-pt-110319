16ff17fa56467ec47d7aa2aec4207308d598b691
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-16 19:47:54 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,9 @@
 class User 
 
-attr_accessor :first_name, :last_name   
+  attr_accessor :first_name, :last_name   
 
-def initialized (first_name,last_name)
-  @first_name = first_name
-  @last_name = last_name
-end 
+  def initialized (first_name,last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end 
 end 
